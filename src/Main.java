66a494aa38a59be898c043b5cd9852a8f71ae71f
BAVDE1 <bavde4@gmail.com>
2025-09-04 12:13:16 +1000

    proper matrix library

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import org.ejml.simple.SimpleMatrix;
+
 import java.util.Arrays;
 
 public class Main {
     static int[] n = new int[] {2, 3, 3, 1};
-    static Matrix[] W = new Matrix[] {
-            new Matrix(n[1], n[0]).randomise(),
-            new Matrix(n[2], n[1]).randomise(),
-            new Matrix(n[3], n[2]).randomise()
+    static SimpleMatrix[] W = new SimpleMatrix[] {
+            SimpleMatrix.random(n[1], n[0]),
+            SimpleMatrix.random(n[2], n[1]),
+            SimpleMatrix.random(n[3], n[2])
     };
-    static Matrix[] b = new Matrix[] {
-            new Matrix(n[1], 1).randomise(),
-            new Matrix(n[2], 1).randomise(),
-            new Matrix(n[3], 1).randomise()
+    static SimpleMatrix[] b = new SimpleMatrix[] {
+            SimpleMatrix.random(n[1], 1),
+            SimpleMatrix.random(n[2], 1),
+            SimpleMatrix.random(n[3], 1)
     };
 
     public static void main(String[] args) {
