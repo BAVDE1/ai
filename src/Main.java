046d5a84296ad1cb4f88e6e40a88f6b7e03a6a68
BAVDE1 <bavde4@gmail.com>
2025-09-04 15:29:19 +1000

    erm what

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import org.ejml.equation.Equation;
 import org.ejml.simple.SimpleMatrix;
 
 public class Main {
+    static final double EPSILON = 0.01;
+
     static int m = 10;
     static int[] n = new int[]{2, 3, 3, 1};
 
@@ -19,36 +21,22 @@ public class Main {
             MatrixUtils.broadcast(MatrixUtils.randomGaussianMat(n[3], 1), m)
     };
 
-    // input data
-    static SimpleMatrix X = inputData();  // m by n0
-    static SimpleMatrix A0 = X.transpose();  // A^[0] `n0 by m`
-
     // training labels
     static SimpleMatrix y = trainingData();  // n3 by m
     static SimpleMatrix Y = y.transpose();  // m by n3
 
     public static SimpleMatrix inputData() {
         SimpleMatrix out = SimpleMatrix.ones(m, n[0]);
-        out.set(0, 0, 150);
-        out.set(0, 1, 70);
-        out.set(1, 0, 254);
-        out.set(1, 1, 73);
-        out.set(2, 0, 312);
-        out.set(2, 1, 68);
-        out.set(3, 0, 120);
-        out.set(3, 1, 60);
-        out.set(4, 0, 154);
-        out.set(4, 1, 61);
-        out.set(5, 0, 212);
-        out.set(5, 1, 65);
-        out.set(6, 0, 216);
-        out.set(6, 1, 67);
-        out.set(7, 0, 145);
-        out.set(7, 1, 67);
-        out.set(8, 0, 184);
-        out.set(8, 1, 64);
-        out.set(9, 0, 130);
-        out.set(9, 1, 69);
+        out.set(0, 0, 150);out.set(0, 1, 70);
+        out.set(1, 0, 254);out.set(1, 1, 73);
+        out.set(2, 0, 312);out.set(2, 1, 68);
+        out.set(3, 0, 120);out.set(3, 1, 60);
+        out.set(4, 0, 154);out.set(4, 1, 61);
+        out.set(5, 0, 212);out.set(5, 1, 65);
+        out.set(6, 0, 216);out.set(6, 1, 67);
+        out.set(7, 0, 145);out.set(7, 1, 67);
+        out.set(8, 0, 184);out.set(8, 1, 64);
+        out.set(9, 0, 130);out.set(9, 1, 69);
         return out;
     }
 
@@ -68,8 +56,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        SimpleMatrix yHat = feedForward();
+        SimpleMatrix yHat = feedForward(inputData());
         double cost = calcCost(yHat);
+
+        MatrixUtils.printMatrixArray(yHat.toArray2());
         System.out.println(cost);
     }
 
@@ -80,7 +70,9 @@ public class Main {
         return eq.lookupSimple("out");
     }
 
-    public static SimpleMatrix feedForward() {
+    public static SimpleMatrix feedForward(SimpleMatrix input) {
+        SimpleMatrix A0 = input.transpose();  // from m by n[0] to n0 by m
+
         SimpleMatrix Z1 = W[1].mult(A0).plus(b[1]);
         SimpleMatrix A1 = sigmoid(Z1);
 
@@ -88,11 +80,7 @@ public class Main {
         SimpleMatrix A2 = sigmoid(Z2);
 
         SimpleMatrix Z3 = W[3].mult(A2).plus(b[3]);
-        SimpleMatrix A3 = sigmoid(Z3);
-
-        System.out.println(A3);
-        MatrixUtils.printMatrixArray(A3.toArray2());
-        return A3;
+        return sigmoid(Z3);
     }
 
     public static double calcCost(SimpleMatrix yHat) {
@@ -101,25 +89,8 @@ public class Main {
             double yI = Y.get(i);
             double yHatI = yHat.get(i);
             double c = yI == 0 ? 1 - yHatI : yHatI;
-//            if (yI == 0) {
-//                c = 1 - yHatI;
-//            } else {
-//                c = yHatI;
-//            }
             summed -= Math.log(c);
-//            System.out.println(yI);
-//            System.out.println(yHatI);
-//            double c = yI * Math.log(yHatI + (1 - yI) * Math.log(1 - yHatI));
-//            System.out.println(c);
-//            summed -= c;
         }
-//        System.out.println(summed);
-        //        Equation eq = new Equation();
-//        eq.alias(yHat, "yHat", Y, "Y");
-//        eq.process("out = -( (Y * log(yHat) + (1 - Y)*log(1 - yHat)) )");
-//        SimpleMatrix losses = eq.lookupSimple("out");
-//        SimpleMatrix m = losses.reshape(-1);
-//        double summed =
         return (1d / m) * summed;
     }
 
